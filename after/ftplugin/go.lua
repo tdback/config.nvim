724fea5d8e506882b8e665574649a6c613e68a64
tdback <tyler@tdback.net>
2025-04-04 22:44:26 -0400

    run gofmt when saving the buffer

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,3 +1,8 @@
 vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
 vim.opt_local.expandtab = false
+
+-- Always run `gofmt` before saving the buffer.
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  callback = function() vim.lsp.buf.format() end,
+})
